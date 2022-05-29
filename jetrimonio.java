75c82736ccf896ed595127d4b0cfe9de5f2ae69c
Jieruei Chang <knosmos@gmail.com>
2022-05-28 20:07:31 -0400

    Initial commit

---
+++ jetrimonio.java
@@ -0,0 +1,37 @@
+public class jetrimonio {
+    private int[] color;
+    private int[][][] layouts = {
+        {
+            {" "," "," "," "},
+            {"o","o","o","o"},
+            {" "," "," "," "},
+            {" "," "," "," "}
+        },
+        {
+            {" ","o"," "," "},
+            {" ","o"," "," "},
+            {" ","o"," "," "},
+            {" ","o"," "," "}
+        }
+    };
+
+    public jetrimonio() {
+
+    }
+
+    public void spin() {
+
+    }
+
+    public void render() {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void changeColor() {
+
+    }
+}
